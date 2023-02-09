4a6317d1ae08ea672cc69b26f88ce9c082053157
vipansh <vipanshthakur@gmail.com>
2023-02-09 13:47:56 +0530

    form button ui foixed

--- components/form/index.tsx
+++ components/form/index.tsx
@@ -252,10 +252,10 @@ const Form = ({ closeModal }: Props) => {
 
         {/* ----------------------old code------------------------ */}
         <div className="divide-y divide-gray-200">
-          <div className="pt-4 flex items-center space-x-4">
+          <div className="pt-4 flex items-center space-x-4 justify-end">
             <button
               onClick={closeModal}
-              className="flex justify-center items-center w-full text-gray-900 px-4 py-3 rounded-md focus:outline-none"
+              className="flex rounded border border-current px-4 py-2 text-sm font-medium text-red-600 transition hover:rotate-2 hover:scale-110 focus:outline-none focus:ring active:text-red-500"
             >
               <svg
                 className="w-6 h-6 mr-3"
@@ -275,7 +275,7 @@ const Form = ({ closeModal }: Props) => {
             </button>
             <button
               type="submit"
-              className="bg-blue-500 flex justify-center items-center w-full text-white px-4 py-3 rounded-md focus:outline-none"
+              className="flex rounded bg-indigo-600 px-12 py-3 text-sm font-medium text-white transition hover:rotate-2 hover:scale-110 focus:outline-none focus:ring active:bg-indigo-500"
               onClick={createData}
             >
               Create
