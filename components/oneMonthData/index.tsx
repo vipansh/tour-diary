ca0d79800938ce895641fe64cd5068f03f8096a6
vipansh <vipanshthakur@gmail.com>
2023-02-09 14:50:57 +0530

    prtin buttion ui fix

--- components/oneMonthData/index.tsx
+++ components/oneMonthData/index.tsx
@@ -8,14 +8,16 @@ type Props = {};
 const OneMonthData = (props: Props) => {
   const [showTourDiary, setShowTourDiary] = useState(false);
   return (
-    <div className="w-10/12 mx-auto">
+    <div className="mx-auto shadow-md bg-gray-900 text-white h-screen py-8">
       <Navbar
         selectedIndex={showTourDiary ? 0 : 1}
         toggleShow={() => {
           setShowTourDiary(!showTourDiary);
         }}
       />
-      {showTourDiary ? <TourDiary /> : <TR7 />}
+      <div className="bg-white text-gray-900  h-screen">
+        {showTourDiary ? <TourDiary /> : <TR7 />}
+      </div>
     </div>
   );
 };
