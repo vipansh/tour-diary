0a26d83f9c9c0f22127587e218a4a141758a88a1
vipansh <vipanshthakur@gmail.com>
2023-02-12 02:37:39 +0530

    type upgraded

--- data/tourDiary.types.ts
+++ data/tourDiary.types.ts
@@ -1,42 +1,33 @@
 export type OneDayDetailsProps = {
   date?: string; //"2022-06-15",
   startingPoint?: {
-    date?: string;
-    name?: string; //"New York City";
     startTime?: string; // "8?:00 AM";
     endTime?: string; // "10?:00 AM";
+    date?: string;
+    name?: string;
+    startingPoint?: string;
+    endPoint?: string;
+    distance?: string;
+    distanceByBus?: number; //3000,
+    distanceOnFoot?: number; //10,
+    note?: string;
   };
   endPoint?: {
-    date?: string;
-    name?: string; //"New York City";
     startTime?: string; // "8?:00 AM";
     endTime?: string; // "10?:00 AM";
+    date?: string;
+    name?: string;
+    startingPoint?: string;
+    endPoint?: string;
+    distance?: string;
+    distanceByBus?: number; //3000,
+    distanceOnFoot?: number; //10,
+    note?: string;
   };
   distanceByBus?: number; //3000,
   distanceOnFoot?: number; //10,
   note?: string;
   isCustom?: string;
-};
-
-export type OneDayDetailsSpecialProps = {
-  startPoint: {
-    date?: string;
-    startingPoint?: string;
-    endPoint?: string;
-    distance?: string;
-    distanceByBus?: number; //3000,
-    distanceOnFoot?: number; //10,
-    note?: string;
-  };
-  endPoint: {
-    date?: string;
-    startingPoint?: string;
-    endPoint?: string;
-    distance?: string;
-    distanceByBus?: number; //3000,
-    distanceOnFoot?: number; //10,
-    note?: string;
-  };
   totalDays?: string;
 };
 
